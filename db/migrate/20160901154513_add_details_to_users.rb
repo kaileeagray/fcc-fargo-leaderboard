ee75a51f1cf8a8950e15e1aa90534044ca773949
kaileeagray <kaileeagray@gmail.com>
2016-09-01 11:23:20 -0500

    Change data type of fcc_username in users table

--- db/migrate/20160901154513_add_details_to_users.rb
+++ db/migrate/20160901154513_add_details_to_users.rb
@@ -2,7 +2,7 @@ class AddDetailsToUsers < ActiveRecord::Migration[5.0]
   def change
    add_column :users, :name, :string
    add_column :users, :role, :integer
-   add_column :users, :fcc_username, :datetime
+   add_column :users, :fcc_username, :string
    add_column :users, :bio, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160901154513) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role"
-    t.datetime "fcc_username"
+    t.string   "fcc_username"
     t.text     "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
